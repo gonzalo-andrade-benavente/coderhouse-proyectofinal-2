8a36fc4a50aec9130edcfeece1ddd08e1230438f
Gonzalo Andrade <gonzalo.andrade@gmail.com>
2022-02-28 21:07:25 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -5,20 +5,21 @@ const app = express();
 const { config } = require('../config');
 const databaseConnection = require('../config/databaseConnection');
 
-const productRouter = require('../routes/product');
-const cartRouter = require('../routes/cart');
+//const productRouter = require('../routes/product');
+//const cartRouter = require('../routes/cart');
 
 const serverRoutes = require('../routes');
+
+// DB
+( async () => {
+    await databaseConnection.connect();
+})();
 
 // MDW
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static(__dirname + '/public'));
 
-// DB
-( async () => {
-    await databaseConnection.connect();
-})();
 
 // CORS
 app.use(cors(config.cors));
